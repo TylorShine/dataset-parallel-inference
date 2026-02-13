0960b6d91078101d89b865cca8cea3a16ad716a6
TylorShine <ricka.tylor@gmail.com>
2026-02-14 00:10:15 +0900

    update: add logic that large JSON split inference
    - especially when there are many keys in "tags"

--- projects/example_3_writing/task.py
+++ projects/example_3_writing/task.py
@@ -10,11 +10,12 @@ from datasets import load_dataset
 from dotenv import load_dotenv
 from openai import AsyncOpenAI, OpenAIError
 from openai.types.chat import ChatCompletionSystemMessageParam, ChatCompletionUserMessageParam
+from pydantic import BaseModel
 
 from core import InferenceTask
 from asyncio import Semaphore
 
-from projects.example_3_chat.model import RootModel
+from projects.example_3_chat.model import RootModel, Prompt, Rubric
 
 
 class Task(InferenceTask):
@@ -25,7 +26,11 @@ class Task(InferenceTask):
         self.dataset = load_dataset("NovelHacja/RubricHub_v1_config", "writing", split="train", streaming=False)
         self._cur.execute(f"CREATE TABLE IF NOT EXISTS result(id INT PRIMARY KEY,content TEXT,source TEXT,reasoning TEXT);")
         self._replace_keys = ["prompt", "reward_model", "Rubrics:reward_model.rubrics"]
-        self._chat_strings = [
+        self._long_str_threshold = 2500
+        load_dotenv(path.join(dirname(__file__), ".env"))
+        self._client = AsyncOpenAI(api_key=os.environ["API_KEY"], base_url=os.environ["BASE_URL"], timeout=None)
+        self._temperature = 0.5
+        self._full_chat_strings = [
             '続くJSONのデータのフィールド`"content"`と`"criterion"`の値のみを、構造をそのままに、全ての文を省略無しで正確に日本語へと翻訳してください。' +
                 'ただし、特に日本語に翻訳したことによって生じる可能性もある`"criterion"`の矛盾は解決してください。' +
                 '`"criterion"`の内容は"`"content"`の回答に対する評価基準"です。JSONのみ出力すること。:\n\n{input_json_str}',
@@ -36,9 +41,27 @@ class Task(InferenceTask):
                 'ただし、`"criterion"`に矛盾がある場合は解決してください。' +
                 '`"criterion"`の内容は"`"content"`に対する回答の質を判断するための評価基準"です。JSONのみ出力すること。:\n\n{input_json_str}',
         ]
-        load_dotenv(path.join(dirname(__file__), ".env"))
-        self._client = AsyncOpenAI(api_key=os.environ["API_KEY"], base_url=os.environ["BASE_URL"], timeout=None)
-        self._temperature = 0.5
+        self._prompt_chat_strings = [
+            '続くJSONのデータのフィールド`"content"`の値のみを、構造をそのままに、全ての文を省略無しで正確に日本語へと翻訳してください。' +
+                'ただし、特に日本語に翻訳したことによって生じる可能性のある矛盾や不自然さは解決してください。JSONのみ出力すること。:\n\n{prompt}',
+            '続くJSONのデータのフィールド`"content"`の値のみを、構造をそのままに、全ての文を省略無しで正確に日本語へと翻訳してください。' +
+                'ただし、文章内で発生している矛盾や不自然さは解決してください。JSONのみ出力すること。:\n\n{prompt}',
+        ]
+        self._criterion_chat_strings = [
+            '続くJSONのデータのフィールド`"criterion"`と`"prompt_to_repeat"`(存在していれば)の値のみを、構造をそのままに、省略無しで正確に日本語へと翻訳してください。' +
+                'ただし、特に日本語に翻訳したことによって生じる可能性のある矛盾や不自然さは解決してください。' +
+                '`"criterion"`の内容は"`プロンプト`の回答に対する評価基準"です。JSONのみ出力すること。\nプロンプト:\n{prompt}\n---\n\nJSON:\n{rubric}',
+            '続くJSONのデータのフィールド`"criterion"`と`"prompt_to_repeat"`(存在していれば)の値のみを、構造をそのままに、省略無しで正確に日本語へと翻訳してください。' +
+                'ただし、`"criterion"`の内容が"`プロンプト`"と矛盾している場合は解決してください。' +
+                '`"criterion"`の内容は"`プロンプト`の回答の質を判断するための評価基準"です。JSONのみ出力すること。\nプロンプト:\n{prompt}\n---\n\nJSON:\n{rubric}',
+        ]
+        if "/" not in os.environ["MODEL_NAME"]:
+            model_provider_text = ""
+            model_name = os.environ["MODEL_NAME"]
+        else:
+            model_provider, model_name = os.environ["MODEL_NAME"].split("/")[:2]
+            model_provider_text = f"{model_provider}製の"
+        self._system_string = f"あなたは{model_provider_text}大規模言語モデル、{model_name}です。広範な知識を伴う言語理解力やユーザ指示への忠実性に秀でており、完全な回答を提供します。"
 
     def get_length(self) -> int:
         # streaming==Falseと仮定
@@ -48,48 +71,24 @@ class Task(InferenceTask):
         self._db.commit()
         self._cur.close()
         self._db.close()
-
-    async def process(self, data, order: int, sem: Semaphore, bar: tqdm.tqdm):
-        # id列に order の値が存在するか確認、したらスキップ
-        if self._cur.execute(f"SELECT COUNT(*) FROM result WHERE id=?;", (order,)).fetchone()[0] > 0:
-            bar.update(1)
-            return
+        
+    async def _process_prompt(self, data, order: int, sem: Semaphore, chat_string_list: list[str], chat_string_format: dict[str, Any], response_format: type[BaseModel], replace_keys: list[str] | None = ["prompt", "reward_model", "Rubrics:reward_model.rubrics"]):
         async with sem:
-            try:
-                # "extra_info" は dict型
-                input_json_str = json.dumps(data["extra_info"], ensure_ascii=False, separators=(",", ":"))
-            except (KeyError, ValueError):
-                # "extra_info" がデコードできない場合は "prompts" と "reward_model", "Rubrics" を取得
-                try:
-                    input_json_str = json.dumps({"prompts": data["prompts"], "reward_model": data["reward_model"], "rubrics": data["Rubrics"]}, ensure_ascii=False, separators=(",", ":"))
-                except (KeyError, ValueError):
-                    # "Rubrics" がデコードできないか存在しない場合は "prompts" と "reward_model" を取得
-                    input_json_str = json.dumps({"prompts": data["prompts"], "reward_model": data["reward_model"]}, ensure_ascii=False, separators=(",", ":"))
-
             original_obj = data.copy()
             translated_obj = None
             reasoning_text = None
 
             if original_obj != None:
                 sleep_time = 2.0
-                if "/" not in os.environ["MODEL_NAME"]:
-                    model_provider_text = ""
-                    model_name = os.environ["MODEL_NAME"]
-                else:
-                    model_provider, model_name = os.environ["MODEL_NAME"].split("/")[:2]
-                    model_provider_text = f"{model_provider}製の"
-                
-                system_string = f"あなたは{model_provider_text}大規模言語モデル、{model_name}です。広範な知識を伴う言語理解力やユーザ指示への忠実性に秀でており、完全な回答を提供します。"
-                
                 max_parse_error_count = 3
                 retry_count_by_parse_error = 0
                 while True:
                     try:
-                        chat_str_index = int(retry_count_by_parse_error // len(self._chat_strings)) % len(self._chat_strings)
-                        chat_string = self._chat_strings[chat_str_index].format(input_json_str=input_json_str)
+                        chat_str_index = int(retry_count_by_parse_error // len(chat_string_list)) % len(chat_string_list)
+                        chat_string = chat_string_list[chat_str_index].format(**chat_string_format)
                         prompt = [
                             ChatCompletionSystemMessageParam(
-                                content=system_string,
+                                content=self._system_string,
                                 role="system"
                             ),
                             ChatCompletionUserMessageParam(
@@ -101,7 +100,7 @@ class Task(InferenceTask):
 
                         resp = await self._client.chat.completions.parse(
                             messages=prompt,
-                            response_format=RootModel,
+                            response_format=response_format,
                             model=os.environ["MODEL_NAME"],
                             temperature=self._temperature,
                             extra_body={"separate_reasoning": True},
@@ -112,7 +111,10 @@ class Task(InferenceTask):
                         translated_resp: dict[str, Any] = resp.choices[0].message.parsed.model_dump()
                         translated_obj = original_obj.copy()
                         
-                        for key in self._replace_keys:
+                        if replace_keys is None:
+                            replace_keys = []
+                            translated_obj = translated_resp.copy()
+                        for key in replace_keys:
                             if key in translated_resp.keys():
                                 translated_obj[key] = translated_resp[key].copy()
                             elif ":" in key:
@@ -138,19 +140,67 @@ class Task(InferenceTask):
                     except OpenAIError as e:
                         if sleep_time > 16.0:
                             translated_obj = {"role": "assistant", "content": "<-- output is missing -->"}
-                            print(f"OpenAI API Error: {e}, retry limit exceeded. index: [{order}]")
+                            print(f"order[{order}]: OpenAI API Error: {e}, retry limit exceeded.")
                             break
                         print(f"OpenAI API Error: {e}")
                         await asyncio.sleep(sleep_time)
                         sleep_time = sleep_time * 2
                     except (json.JSONDecodeError, KeyError, ValueError) as e:
-                        if retry_count_by_parse_error < max_parse_error_count * len(self._chat_strings):
+                        if retry_count_by_parse_error < max_parse_error_count * len(chat_string_list):
                             retry_count_by_parse_error += 1
-                            print(f"Retrying due to parse error: {e}")
+                            print(f"order[{order}]: Retrying due to parse error: {e}")
                             continue
                         translated_obj = {"role": "assistant", "content": "<-- output is missing JSON -->"}
                         print(f"JSON Error: {e}")
                         break
+        return translated_obj, reasoning_text
+
+    async def process(self, data, order: int, sem: Semaphore, bar: tqdm.tqdm):
+        # id列に order の値が存在するか確認、したらスキップ
+        if self._cur.execute(f"SELECT COUNT(*) FROM result WHERE id=?;", (order,)).fetchone()[0] > 0:
+            bar.update(1)
+            return
+        try:
+            # "extra_info" は dict型
+            input_json_str = json.dumps(data["extra_info"], ensure_ascii=False, separators=(",", ":"))
+        except (KeyError, ValueError):
+            # "extra_info" がデコードできない場合は "prompt" と "reward_model", "Rubrics" を取得
+            try:
+                input_json_str = json.dumps({"prompt": data["prompt"], "reward_model": data["reward_model"], "rubrics": data["Rubrics"]}, ensure_ascii=False, separators=(",", ":"))
+            except (KeyError, ValueError):
+                # "Rubrics" がデコードできないか存在しない場合は "prompts" と "reward_model" を取得
+                input_json_str = json.dumps({"prompt": data["prompt"], "reward_model": data["reward_model"]}, ensure_ascii=False, separators=(",", ":"))
+                
+        original_obj = data.copy()
+        
+        is_should_split = len(input_json_str) > self._long_str_threshold
+        # print(f"len(data['Rubrics']): {len(data['Rubrics'])}")
+        # print(f"rubrics: {[v for v in data['Rubrics']]}")
+        len_rubric_parameters_keys_list = [len(rubric["tags"]["parameters"].keys()) for rubric in data["Rubrics"] if "tags" in rubric and rubric["tags"]["parameters"] is not None]
+        is_should_split = is_should_split or (len(len_rubric_parameters_keys_list) > 0 and max(len_rubric_parameters_keys_list) > 8)
+                
+        if len(input_json_str) <= self._long_str_threshold:
+            translated_obj, reasoning_text = await self._process_prompt(data, order, sem, self._full_chat_strings, {"input_json_str": input_json_str}, RootModel, self._replace_keys)
+        else:
+            # long_str_threshold文字以上、またはtagsが8つ以上ついた項目がある場合はプロンプトとチャットを分離する
+            print(f"order[{order}]: is_should_split: {is_should_split}")
+            translated_obj = original_obj.copy()
+            prompt_obj = {"prompt": original_obj["prompt"]}
+            prompt_str = json.dumps(prompt_obj, ensure_ascii=False, separators=(",", ":"))
+            translated_prompt, reasoning_text_prompt = await self._process_prompt(prompt_obj, order, sem, self._prompt_chat_strings, {"prompt": prompt_str}, Prompt, None)
+            translated_obj["prompt"] = translated_prompt["prompt"].copy()
+            reasoning_texts = [reasoning_text_prompt]
+            for idx, rubric in enumerate(original_obj["Rubrics"]):
+                if "verifier" in rubric.keys() and rubric["verifier"] == "rule":
+                    # ruleの場合はそのまま
+                    continue
+                rubric_str = json.dumps(rubric, ensure_ascii=False, separators=(",", ":"))
+                translated_rubric, reasoning_text_rubric = await self._process_prompt(rubric, order, sem, self._criterion_chat_strings, {"prompt": translated_prompt["prompt"][0]["content"], "rubric": rubric_str}, Rubric, None)
+                translated_obj["Rubrics"][idx] = translated_rubric.copy()
+                reasoning_texts.append(reasoning_text_rubric)
+            reasoning_text = json.dumps(reasoning_texts, ensure_ascii=False, separators=(",", ":"))
+            translated_obj["reward_model"]["rubrics"] = translated_obj["Rubrics"]
+        
                 # print(json.dumps(translated_obj, ensure_ascii=False))
         async with self._db_write_sem:
             self._cur.execute(f"REPLACE INTO result(id, content, source, reasoning) VALUES (?,?,?,?);",
